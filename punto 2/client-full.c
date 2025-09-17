96054e313670b70ca94027b840875794fb1e7c35
lucas <lucas@Ubuntu.myguest.virtualbox.org>
2025-09-17 14:14:00 -0300

    reacomodo carpetas e impl. de punto 3

---
+++ punto 2/client-full.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+
+void error(const char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr,"Uso: %s <hostname> <puerto>\n", argv[0]);
+        exit(0);
+    }
+
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR al abrir socket");
+
+    server = gethostbyname(argv[1]);
+    if (!server) {
+        fprintf(stderr,"ERROR, host inexistente\n");
+        exit(0);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+	
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) error("ERROR en connect");
+    
+    
+    
+    clock_t w0, w1, r0, r1;
+    
+    FILE *write_file = fopen("write_time.txt", "a");
+	FILE *read_file = fopen("read_time.txt", "a");
+    
+
+    for (int exp = 1; exp <= 6; exp++) {
+        size_t n = 1;
+        for (int i=0; i<exp; i++) n *= 10;  // 10^exp
+
+        char *buffer = malloc(n);
+        if (!buffer) error("malloc");
+
+        memset(buffer, 'X', n);  // llena con datos 
+
+		
+		// ENVÍO
+		w0 = clock();
+        int w = write(sockfd, buffer, n);
+		w1 = clock();
+		double write_time = ((double)(w1-w0))/CLOCKS_PER_SEC;
+        
+        if (w < 0) error("write");
+
+        printf("Cliente: enviado bloque de 10^%d = %zu bytes\n", exp, w);
+        fprintf(write_file, "%f\n", write_time);
+        
+        
+        
+        // RECEPCIÓN
+        char *buffer_recv = malloc(256);
+        r0 = clock();
+		int r = read(sockfd, buffer_recv, 255);
+		r1 = clock();
+		double read_time = ((double)(r1-r0))/CLOCKS_PER_SEC;
+		
+		
+
+
+		if (r < 0) {
+			error("ERROR reading from socket");
+			break;	 
+		}
+		printf("El mensaje recibido es: %s\n", buffer_recv);
+		fprintf(read_file, "%f\n", read_time);
+
+
+        free(buffer);
+        free(buffer_recv);
+    }
+    
+    close(write_file);
+    close(read_file);
+    close(sockfd);
+    return 0;
+}
