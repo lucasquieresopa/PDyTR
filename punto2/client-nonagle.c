0fa2d98a04fa95e34a9caa26de8ec24f6a78cf87
lucas <lucas@Ubuntu.myguest.virtualbox.org>
2025-09-26 12:10:02 -0300

    p2 sin nagle

---
+++ punto2/client-nonagle.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <netinet/tcp.h>
+
+void error(const char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+double dwalltime(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr,"Uso: %s <hostname> <puerto>\n", argv[0]);
+        exit(0);
+    }
+
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR al abrir socket");
+
+    server = gethostbyname(argv[1]);
+    if (!server) {
+        fprintf(stderr,"ERROR, host inexistente\n");
+        exit(0);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+	
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) error("ERROR en connect");
+    
+    // DESACTVIO NAGLE
+    int flag = 1;
+    if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag)) < 0){
+    	perror("setsockopt TCP_NODELAY");
+    }
+    
+    
+    
+    double w0, w1, r0, r1;
+    
+    FILE *write_file = fopen("write_time.txt", "w");
+	FILE *read_file = fopen("read_time.txt", "w");
+    for(int i = 0; i<6; i++){
+		for (int exp = 1; exp <= 6; exp++) {
+			size_t n = 1;
+			for (int i=0; i<exp; i++) n *= 10;  // 10^exp
+	
+			char *buffer = malloc(n);
+			if (!buffer) error("malloc");
+	
+			memset(buffer, 'X', n);  // llena con datos 
+	
+			
+			// ENVÍO
+			w0 = dwalltime();
+			int w = write(sockfd, buffer, n);
+			w1 = dwalltime();
+			double write_time = w1-w0; // CLOCKS_PER_SEC;
+			
+			if (w < 0) error("write");
+	
+			printf("Cliente: enviado bloque de 10^%d = %zu bytes\n", exp, (size_t) w);
+			fprintf(write_file, "%f\n", write_time);
+			
+			
+			
+			// RECEPCIÓN
+			char *buffer_recv = malloc(256);
+			r0 = dwalltime();
+			int r = read(sockfd, buffer_recv, 255);
+			r1 = dwalltime();
+			double read_time = (r1-r0); //CLOCKS_PER_SEC;
+			
+			
+	
+	
+			if (r < 0) {
+				error("ERROR reading from socket");
+				break;	 
+			}
+			printf("El mensaje recibido es: %s\n", buffer_recv);
+			fprintf(read_file, "%f\n", read_time);
+	
+	
+			free(buffer);
+			free(buffer_recv);
+		}
+		printf("ITER: %d", i+1);
+	}
+    fclose(write_file);
+    fclose(read_file);
+    close(sockfd);
+    return 0;
+}
---
+++ punto2/server-nonagle.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <puerto>\n", argv[0]);
+        exit(1);
+    }
+
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char *buffer;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR al abrir socket");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    
+    // DESACTVIO NAGLE
+    int flag = 1;
+    if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag)) < 0){
+    	perror("setsockopt TCP_NODELAY");
+    }
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR en bind");
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) error("ERROR en accept");
+	for(int i=0; i<6; i++){
+	for (int exp = 1; exp <= 6; exp++) {
+		size_t n = 1;
+		for (int i=0; i<exp; i++) n *= 10;  // 10^exp
+
+		buffer = malloc(n);
+		if (!buffer) error("malloc");
+		
+		size_t total = 0;
+	
+		// En este bucle se leen los datos en varias iteraciones
+		while (total < n){
+			ssize_t r = read(newsockfd, buffer + total, n - total);
+			if (r < 0) error("Read");
+			if (r == 0) break;
+			total += (ssize_t)r;
+			printf("Leyendo cantidad: %zu\n", r);
+		}
+
+		printf("Servidor: recibido bloque de 10^%d = %zu bytes\n", exp, total);
+		
+		
+		// CONFIRMACIÓN DE RECEPCIÓN
+		
+		int w = write(newsockfd, "Se recibio el mensaje\0", 23);
+		if (w < 0) error("ERROR writing to socket");
+
+		free(buffer);
+	}
+	}
+		
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
