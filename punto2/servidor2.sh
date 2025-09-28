6a102e6abe85a4c68ca96330812e16135bb418ff
lucasquieresopa <lucassalanitro32@gmail.com>
2025-09-27 21:12:24 -0300

    nohup a servidor

--- punto2/servidor2.sh
+++ punto2/servidor2.sh
@@ -4,4 +4,4 @@
 echo "PUNTO 2: SERVIDOR" 
 cd /opt/pdytr/punto2
 gcc -o servidor server-full.c
-./servidor 6901 
+nohup ./servidor 6901 > /home/vagrant/servidor_6901.log 2>&1 &
--- punto3/servidor3.sh
+++ punto3/servidor3.sh
@@ -3,4 +3,4 @@
 echo "PUNTO 3: SERVIDOR" 
 cd /opt/pdytr/punto3
 gcc -o servidor server3.c
-./servidor 6902 
+nohup ./servidor 6902 > /home/vagrant/servidor_6902.log 2>&1 &
