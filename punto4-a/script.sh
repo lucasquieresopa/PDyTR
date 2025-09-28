a71ab7caf76a7eb270922119f06b12e218819903
lucasquieresopa <lucassalanitro32@gmail.com>
2025-09-28 10:35:29 -0300

    cambios de directorio en todos los .sh y vf

--- punto4-a/script.sh
+++ punto4-a/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# vagrant up --provision
+vagrant up --provision
 
 # Ejecucion del punto 2
 
-vagrant ssh vm1 -c "/opt/pdytr/punto2/servidor 6901" &
+vagrant ssh vm1 -c "/home/pdytr/punto2/servidor 6901" &
 
 # Esperar en VM1 hasta que el puerto esté en LISTEN (no consume conexiones)
 echo "Esperando a que el servidor escuche en 6901..."
@@ -15,11 +15,11 @@ done
 # Recién ahora lanzar el cliente en VM2
 echo "Servidor listo, arrancando cliente"
 
-vagrant ssh vm2 -c "/opt/pdytr/punto2/cliente 192.168.56.11 6901"
+vagrant ssh vm2 -c "/home/pdytr/punto2/cliente 192.168.56.11 6901"
 
 # Ejecucion del punto 3
 
-vagrant ssh vm1 -c "/opt/pdytr/punto3/servidor 6902" &
+vagrant ssh vm1 -c "/home/pdytr/punto3/servidor 6902" &
 
 # Esperar en VM1 hasta que el puerto esté en LISTEN
 echo "Esperando a que el servidor escuche en 6902..."
@@ -30,4 +30,4 @@ done
 # Recién ahora lanzar el cliente en VM2
 echo "Servidor listo, arrancando cliente"
 
-vagrant ssh vm2 -c "/opt/pdytr/punto3/cliente 192.168.56.11 6902"
+vagrant ssh vm2 -c "/home/pdytr/punto3/cliente 192.168.56.11 6902"
